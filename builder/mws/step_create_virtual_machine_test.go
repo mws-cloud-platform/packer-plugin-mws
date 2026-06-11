647bf25c92b0cb97939375f35a9a45e2fd9b904f
Egor Tkachenko <tkachenko.egor.a@gmail.com>
2026-06-11 12:33:59 +0300

    separate run and cleanup tests

--- builder/mws/step_create_virtual_machine_test.go
+++ builder/mws/step_create_virtual_machine_test.go
@@ -4,6 +4,7 @@ import (
 	"cmp"
 	"context"
 	"errors"
+	"fmt"
 	"path"
 	"testing"
 
@@ -19,7 +20,7 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
-func TestStepCreateVirtualMachine_Success(t *testing.T) {
+func TestStepCreateVirtualMachine_Run_Success(t *testing.T) {
 	t.Parallel()
 	expectedDir := golden.NewDir(t, golden.WithPath(path.Join("testdata", t.Name())), golden.WithRecreateOnUpdate())
 
@@ -128,17 +129,6 @@ func TestStepCreateVirtualMachine_Success(t *testing.T) {
 				}).
 				Times(1)
 
-			driver.EXPECT().DeleteFirewallRule(gomock.Any(), expectedNetworkName, expectedFirewallRuleName).Times(1)
-			driver.EXPECT().DeleteVirtualMachine(gomock.Any(), expectedVirtualMachineName).Times(1)
-			if tt.config.SubnetName == "" {
-				driver.EXPECT().DeleteSubnet(gomock.Any(), expectedNetworkName, expectedSubnetName).Times(1)
-			}
-			if tt.config.NetworkName == "" {
-				driver.EXPECT().DeleteNetwork(gomock.Any(), expectedNetworkName).Times(1)
-			}
-			driver.EXPECT().DeleteExternalAddress(gomock.Any(), expectedExternalAddressName).Times(1)
-			driver.EXPECT().DeleteDisk(gomock.Any(), expectedDiskName).Times(1)
-
 			step := &mws.StepCreateVirtualMachine{
 				GeneratedData: &packerbuilderdata.GeneratedData{State: state},
 			}
@@ -155,64 +145,131 @@ func TestStepCreateVirtualMachine_Success(t *testing.T) {
 			requireStateGet(t, state, mws.DiskRefKey, expectedDiskRef)
 			requireGeneratedDataGet(t, state, "SourceProject", tt.config.Project)
 			requireGeneratedDataGet(t, state, "SourceImageName", testSourceImage)
-			step.Cleanup(state)
 			expectedDir.String(t, tt.name+".out", writer.String())
 		})
 	}
 }
 
-func TestStepCreateVirtualMachine_Error(t *testing.T) {
+func TestStepCreateVirtualMachine_Cleanup_Success(t *testing.T) {
 	t.Parallel()
 	expectedDir := golden.NewDir(t, golden.WithPath(path.Join("testdata", t.Name())), golden.WithRecreateOnUpdate())
 
 	for _, tt := range []struct {
-		name               string
-		expectedActionHalt bool
+		name   string
+		config *mws.Config
 	}{
 		{
-			name:               "CreateDisk",
-			expectedActionHalt: true,
+			name: "all_set",
+			config: &mws.Config{
+				Project:             testProjectName,
+				DiskName:            testDiskName,
+				NetworkName:         testNetworkName,
+				SubnetName:          testSubnetName,
+				ExternalAddressName: testExternalAddressName,
+				VirtualMachineName:  testVirtualMachineName,
+				SourceImage:         testSourceImage,
+			},
 		},
 		{
-			name:               "CreateExternalAddress",
-			expectedActionHalt: true,
+			name: "network_set",
+			config: &mws.Config{
+				Project:     testProjectName,
+				SourceImage: testSourceImage,
+				NetworkName: testNetworkName,
+			},
 		},
 		{
-			name:               "CreateNetwork",
-			expectedActionHalt: true,
-		},
-		{
-			name:               "CreateSubnet",
-			expectedActionHalt: true,
-		},
-		{
-			name:               "CreateVirtualMachine",
-			expectedActionHalt: true,
-		},
-		{
-			name:               "CreateFirewallRule",
-			expectedActionHalt: true,
-		},
-		{
-			name: "DeleteFirewallRule",
-		},
-		{
-			name: "DeleteVirtualMachine",
-		},
-		{
-			name: "DeleteSubnet",
-		},
-		{
-			name: "DeleteNetwork",
-		},
-		{
-			name: "DeleteExternalAddress",
-		},
-		{
-			name: "DeleteDisk",
+			name: "all_default",
+			config: &mws.Config{
+				Project:     testProjectName,
+				SourceImage: testSourceImage,
+			},
 		},
 	} {
-		t.Run(tt.name, func(t *testing.T) {
+		for _, errorInRun := range []string{
+			"CreateDisk",
+			"CreateExternalAddress",
+			"CreateNetwork",
+			"CreateSubnet",
+			"CreateVirtualMachine",
+			"CreateFirewallRule",
+			"None",
+		} {
+			t.Run(tt.name, func(t *testing.T) {
+				ctrl := gomock.NewController(t)
+				defer ctrl.Finish()
+
+				driver := mockmws.NewMockDriver(ctrl)
+				writer, state := prepareState(t, tt.config, driver)
+
+				expectedDiskName := cmp.Or(tt.config.DiskName, defaultDiskName)
+				expectedExternalAddressName := cmp.Or(tt.config.ExternalAddressName, defaultExternalAddressName)
+				expectedNetworkName := cmp.Or(tt.config.NetworkName, defaultNetworkName)
+				expectedSubnetName := cmp.Or(tt.config.SubnetName, defaultSubnetName)
+				expectedVirtualMachineName := cmp.Or(tt.config.VirtualMachineName, defaultVirtualMachineName)
+				expectedFirewallRuleName := mws.FirewallRuleName
+
+				func() {
+					if errorInRun == "CreateDisk" {
+						return
+					}
+					state.Put(mws.DiskNameKey, expectedDiskName)
+					driver.EXPECT().DeleteDisk(gomock.Any(), expectedDiskName).Times(1)
+					if errorInRun == "CreateExternalAddress" {
+						return
+					}
+					state.Put(mws.ExternalAddressNameKey, expectedExternalAddressName)
+					driver.EXPECT().DeleteExternalAddress(gomock.Any(), expectedExternalAddressName).Times(1)
+					if errorInRun == "CreateNetwork" {
+						return
+					}
+					state.Put(mws.NetworkNameKey, expectedNetworkName)
+					if tt.config.NetworkName == "" {
+						driver.EXPECT().DeleteNetwork(gomock.Any(), expectedNetworkName).Times(1)
+					}
+					if errorInRun == "CreateSubnet" {
+						return
+					}
+					state.Put(mws.SubnetNameKey, expectedSubnetName)
+					if tt.config.SubnetName == "" {
+						driver.EXPECT().DeleteSubnet(gomock.Any(), expectedNetworkName, expectedSubnetName).Times(1)
+					}
+					if errorInRun == "CreateVirtualMachine" {
+						return
+					}
+					state.Put(mws.VirtualMachineNameKey, expectedVirtualMachineName)
+					driver.EXPECT().DeleteVirtualMachine(gomock.Any(), expectedVirtualMachineName).Times(1)
+					if errorInRun == "CreateFirewallRule" {
+						return
+					}
+					state.Put(mws.FirewallRuleNameKey, expectedFirewallRuleName)
+					driver.EXPECT().DeleteFirewallRule(gomock.Any(), expectedNetworkName, expectedFirewallRuleName).Times(1)
+				}()
+
+				step := &mws.StepCreateVirtualMachine{
+					GeneratedData: &packerbuilderdata.GeneratedData{State: state},
+				}
+
+				step.Cleanup(state)
+				expectedDir.String(t, fmt.Sprintf("%s_with_%s_error_in_run.out", tt.name, errorInRun), writer.String())
+			})
+		}
+	}
+}
+
+func TestStepCreateVirtualMachine_Run_Error(t *testing.T) {
+	t.Parallel()
+	expectedDir := golden.NewDir(t, golden.WithPath(path.Join("testdata", t.Name())), golden.WithRecreateOnUpdate())
+
+	for _, testName := range []string{
+		"CreateDisk",
+		"CreateExternalAddress",
+		"CreateNetwork",
+		"CreateSubnet",
+		"CreateVirtualMachine",
+		"CreateFirewallRule",
+	} {
+		t.Run(testName, func(t *testing.T) {
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
 
@@ -226,64 +283,147 @@ func TestStepCreateVirtualMachine_Error(t *testing.T) {
 				driver,
 			)
 
-			expectedErrors := map[string]error{tt.name: errors.New("test error")}
+			expectedDiskName := defaultDiskName
+			expectedExternalAddressName := defaultExternalAddressName
+			expectedNetworkName := defaultNetworkName
+			expectedSubnetName := defaultSubnetName
+			expectedVirtualMachineName := defaultVirtualMachineName
+			expectedFirewallRuleName := mws.FirewallRuleName
+			expectedDiskRef := new(computeref.NewDiskRef(testProjectName, expectedDiskName))
+
+			expectedErrors := map[string]error{testName: errors.New("test error")}
 			func() {
 				driver.EXPECT().CreateDisk(gomock.Any(), gomock.Any()).
 					Return(expectedErrors["CreateDisk"]).Times(1)
-				if tt.name == "CreateDisk" {
+				if testName == "CreateDisk" {
 					return
 				}
-				driver.EXPECT().DeleteDisk(gomock.Any(), gomock.Any()).
-					Return(expectedErrors["DeleteDisk"]).Times(1)
 				driver.EXPECT().CreateExternalAddress(gomock.Any(), gomock.Any()).
 					Return(testExternalAddress, expectedErrors["CreateExternalAddress"]).Times(1)
-				if tt.name == "CreateExternalAddress" {
+				if testName == "CreateExternalAddress" {
 					return
 				}
-				driver.EXPECT().DeleteExternalAddress(gomock.Any(), gomock.Any()).
-					Return(expectedErrors["DeleteExternalAddress"]).Times(1)
 				driver.EXPECT().CreateNetwork(gomock.Any(), gomock.Any()).
 					Return(expectedErrors["CreateNetwork"]).Times(1)
-				if tt.name == "CreateNetwork" {
+				if testName == "CreateNetwork" {
 					return
 				}
-				driver.EXPECT().DeleteNetwork(gomock.Any(), gomock.Any()).
-					Return(expectedErrors["DeleteNetwork"]).Times(1)
 				driver.EXPECT().CreateSubnet(gomock.Any(), gomock.Any()).
 					Return(expectedErrors["CreateSubnet"]).Times(1)
-				if tt.name == "CreateSubnet" {
+				if testName == "CreateSubnet" {
 					return
 				}
-				driver.EXPECT().DeleteSubnet(gomock.Any(), gomock.Any(), gomock.Any()).
-					Return(expectedErrors["DeleteSubnet"]).Times(1)
 				driver.EXPECT().CreateVirtualMachine(gomock.Any(), gomock.Any()).
 					Return(testInternalAddress, expectedErrors["CreateVirtualMachine"]).Times(1)
-				if tt.name == "CreateVirtualMachine" {
+				if testName == "CreateVirtualMachine" {
 					return
 				}
-				driver.EXPECT().DeleteVirtualMachine(gomock.Any(), gomock.Any()).
-					Return(expectedErrors["DeleteVirtualMachine"]).Times(1)
 				driver.EXPECT().CreateFirewallRule(gomock.Any(), gomock.Any()).
 					Return(expectedErrors["CreateFirewallRule"]).Times(1)
-				if tt.name == "CreateFirewallRule" {
-					return
-				}
-				driver.EXPECT().DeleteFirewallRule(gomock.Any(), gomock.Any(), gomock.Any()).
-					Return(expectedErrors["DeleteFirewallRule"]).Times(1)
 			}()
 
 			step := &mws.StepCreateVirtualMachine{
 				GeneratedData: &packerbuilderdata.GeneratedData{State: state},
 			}
 
-			action := step.Run(context.Background(), state)
-			if tt.expectedActionHalt {
-				requireActionHalt(t, state, action)
-			} else {
-				requireActionContinue(t, state, action)
+			requireActionHalt(t, state, step.Run(context.Background(), state))
+
+			func() {
+				if testName == "CreateDisk" {
+					return
+				}
+				requireStateGet(t, state, mws.DiskNameKey, expectedDiskName)
+				if testName == "CreateExternalAddress" {
+					return
+				}
+				requireStateGet(t, state, mws.ExternalAddressNameKey, expectedExternalAddressName)
+				if testName == "CreateNetwork" {
+					return
+				}
+				requireStateGet(t, state, mws.NetworkNameKey, expectedNetworkName)
+				if testName == "CreateSubnet" {
+					return
+				}
+				requireStateGet(t, state, mws.SubnetNameKey, expectedSubnetName)
+				if testName == "CreateVirtualMachine" {
+					return
+				}
+				requireStateGet(t, state, mws.VirtualMachineNameKey, expectedVirtualMachineName)
+				if testName == "CreateFirewallRule" {
+					return
+				}
+				requireStateGet(t, state, mws.FirewallRuleNameKey, expectedFirewallRuleName)
+
+				requireStateGet(t, state, mws.DiskRefKey, expectedDiskRef)
+				requireStateGet(t, state, mws.InstanceIpKey, testExternalAddress)
+				requireStateGet(t, state, mws.InstanceIdKey, expectedVirtualMachineName)
+			}()
+
+			expectedDir.String(t, testName+".out", writer.String())
+		})
+	}
+}
+
+func TestStepCreateVirtualMachine_Cleanup_Error(t *testing.T) {
+	t.Parallel()
+	expectedDir := golden.NewDir(t, golden.WithPath(path.Join("testdata", t.Name())), golden.WithRecreateOnUpdate())
+
+	for _, testName := range []string{
+		"DeleteFirewallRule",
+		"DeleteVirtualMachine",
+		"DeleteSubnet",
+		"DeleteNetwork",
+		"DeleteExternalAddress",
+		"DeleteDisk",
+	} {
+		t.Run(testName, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			driver := mockmws.NewMockDriver(ctrl)
+
+			writer, state := prepareState(t,
+				&mws.Config{
+					Project:     testProjectName,
+					SourceImage: testSourceImage,
+				},
+				driver,
+			)
+
+			expectedDiskName := defaultDiskName
+			expectedExternalAddressName := defaultExternalAddressName
+			expectedNetworkName := defaultNetworkName
+			expectedSubnetName := defaultSubnetName
+			expectedVirtualMachineName := defaultVirtualMachineName
+			expectedFirewallRuleName := mws.FirewallRuleName
+
+			state.Put(mws.DiskNameKey, expectedDiskName)
+			state.Put(mws.ExternalAddressNameKey, expectedExternalAddressName)
+			state.Put(mws.NetworkNameKey, expectedNetworkName)
+			state.Put(mws.SubnetNameKey, expectedSubnetName)
+			state.Put(mws.VirtualMachineNameKey, expectedVirtualMachineName)
+			state.Put(mws.FirewallRuleNameKey, expectedFirewallRuleName)
+
+			expectedErrors := map[string]error{testName: errors.New("test error")}
+			driver.EXPECT().DeleteFirewallRule(gomock.Any(), gomock.Any(), gomock.Any()).
+				Return(expectedErrors["DeleteFirewallRule"]).Times(1)
+			driver.EXPECT().DeleteVirtualMachine(gomock.Any(), gomock.Any()).
+				Return(expectedErrors["DeleteVirtualMachine"]).Times(1)
+			driver.EXPECT().DeleteSubnet(gomock.Any(), gomock.Any(), gomock.Any()).
+				Return(expectedErrors["DeleteSubnet"]).Times(1)
+			driver.EXPECT().DeleteNetwork(gomock.Any(), gomock.Any()).
+				Return(expectedErrors["DeleteNetwork"]).Times(1)
+			driver.EXPECT().DeleteExternalAddress(gomock.Any(), gomock.Any()).
+				Return(expectedErrors["DeleteExternalAddress"]).Times(1)
+			driver.EXPECT().DeleteDisk(gomock.Any(), gomock.Any()).
+				Return(expectedErrors["DeleteDisk"]).Times(1)
+
+			step := &mws.StepCreateVirtualMachine{
+				GeneratedData: &packerbuilderdata.GeneratedData{State: state},
 			}
+
 			step.Cleanup(state)
-			expectedDir.String(t, tt.name+".out", writer.String())
+			expectedDir.String(t, testName+".out", writer.String())
 		})
 	}
 }
