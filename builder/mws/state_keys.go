144a817002645a37c28383795fbac292cb5ce9b1
Lev Zakharov <l.j.zakharov@gmail.com>
2026-06-29 19:44:46 +0300

    add upload aws credentials step for export post-processor (#16)

--- builder/mws/state_keys.go
+++ builder/mws/state_keys.go
@@ -4,12 +4,13 @@
 package mws
 
 const (
-	ConfigKey     = "config"
-	DriverKey     = "driver"
-	HookKey       = "hook"
-	UIKey         = "ui"
-	UUIDPrefixKey = "uuid_prefix"
-	ErrorKey      = "error"
+	ConfigKey       = "config"
+	DriverKey       = "driver"
+	HookKey         = "hook"
+	UIKey           = "ui"
+	CommunicatorKey = "communicator"
+	UUIDPrefixKey   = "uuid_prefix"
+	ErrorKey        = "error"
 
 	DiskNameKey            = "disk_name"
 	ExternalAddressNameKey = "external_address_name"
