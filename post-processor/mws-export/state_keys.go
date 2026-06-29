696c33c26430116d17e2f692ccf3b09c95de1fce
Lev Zakharov <l.j.zakharov@gmail.com>
2026-06-29 16:10:48 +0300

    add upload aws credentials step for export post-processor

---
+++ post-processor/mws-export/state_keys.go
@@ -0,0 +1,9 @@
+// Copyright 2026 MTS Web Services, LLC.
+// SPDX-License-Identifier: MPL-2.0
+
+package mwsexport
+
+const (
+	HMACAccessKeyStateKey = "hmac_access_key"
+	HMACSecretKeyStateKey = "hmac_secret_key"
+)
---
+++ post-processor/mws-export/step_upload_aws_credentials.go
@@ -0,0 +1,40 @@
+// Copyright 2026 MTS Web Services, LLC.
+// SPDX-License-Identifier: MPL-2.0
+
+package mwsexport
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/packer-plugin-sdk/multistep"
+	"github.com/hashicorp/packer-plugin-sdk/packer"
+	"github.com/mws-cloud-platform/packer-plugin-mws/builder/mws"
+)
+
+const awsSharedCredsFile = "/tmp/aws_credentials" //nolint:gosec // no hardcoded credentials, only path
+
+type StepUploadAWSSharedCredsFile struct {
+}
+
+func (s *StepUploadAWSSharedCredsFile) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {
+	ui := state.Get(mws.UIKey).(packer.Ui)
+	comm := state.Get(mws.CommunicatorKey).(packer.Communicator)
+	hmacAccessKey := state.Get(HMACAccessKeyStateKey).(string)
+	hmacSecretKey := state.Get(HMACSecretKeyStateKey).(string)
+
+	ui.Say("Uploading AWS shared credentials file...")
+	creds := fmt.Sprintf(
+		"[default]\naws_access_key_id = %s\naws_secret_access_key = %s\n",
+		hmacAccessKey, hmacSecretKey,
+	)
+	if err := comm.Upload(awsSharedCredsFile, strings.NewReader(creds), nil); err != nil {
+		return mws.ActionHaltWithError(state, err)
+	}
+
+	ui.Say("AWS shared credentials file uploaded")
+	return multistep.ActionContinue
+}
+
+func (s *StepUploadAWSSharedCredsFile) Cleanup(state multistep.StateBag) {}
