15888bc3aec898e02a2d922eadfccde08b5e3034
Egor Tkachenko <tkachenko.egor.a@gmail.com>
2026-08-18 16:06:37 +0300

    acceptance test for builder

--- builder/mws/builder_acc_test.go
+++ builder/mws/builder_acc_test.go
@@ -4,30 +4,53 @@
 package mws
 
 import (
-	_ "embed"
 	"fmt"
 	"os/exec"
 	"testing"
 
 	"github.com/hashicorp/packer-plugin-sdk/acctest"
+	"github.com/hashicorp/packer-plugin-sdk/random"
+	"github.com/mws-cloud-platform/packer-plugin-mws/example"
+	"github.com/stretchr/testify/require"
+	"go.mws.cloud/go-sdk/mws"
+	"go.mws.cloud/go-sdk/service/compute/client"
+	computesdk "go.mws.cloud/go-sdk/service/compute/sdk"
 )
 
-//go:embed test-fixtures/template.pkr.hcl
-var testBuilderHCL2Basic string
-
 func TestAccMWSBuilder(t *testing.T) {
+	ctx := t.Context()
+	sdk, err := mws.Load(ctx)
+	require.NoError(t, err)
+	imageClient, err := computesdk.NewImage(ctx, sdk)
+	require.NoError(t, err)
+
+	imageName := fmt.Sprintf("packer-acctest-%s-image", random.AlphaNumLower(6))
+
 	testCase := &acctest.PluginTestCase{
-		Name:     "mws_builder_basic_test",
-		Template: testBuilderHCL2Basic,
-		Type:     "mws",
+		Name:           "builder_example",
+		Template:       example.BuilderHCL,
+		Type:           "mws",
+		BuildExtraArgs: []string{"-var", "image_name=" + imageName},
 		Check: func(buildCommand *exec.Cmd, logfile string) error {
-			if buildCommand.ProcessState != nil {
-				if buildCommand.ProcessState.ExitCode() != 0 {
-					return fmt.Errorf("Bad exit code. Logfile: %s", logfile)
-				}
+			if buildCommand.ProcessState != nil && buildCommand.ProcessState.ExitCode() != 0 {
+				return fmt.Errorf("Bad exit code. Logfile: %s", logfile)
+			}
+			if _, err := imageClient.GetImage(ctx, client.GetImageRequest{
+				Image: imageName,
+			}, client.WithWait()); err != nil {
+				return fmt.Errorf("get image: %w", err)
+			}
+			return nil
+		},
+		Teardown: func() error {
+			if err := imageClient.DeleteImage(ctx, client.DeleteImageRequest{
+				Image: imageName,
+			}, client.WithWait()); err != nil {
+				return fmt.Errorf("delete image: %w", err)
 			}
 			return nil
 		},
 	}
+
 	acctest.TestPlugin(t, testCase)
 }
---
+++ example/assets.go
@@ -0,0 +1,11 @@
+// Copyright 2026 MTS Web Services, LLC.
+// SPDX-License-Identifier: MPL-2.0
+
+package example
+
+import (
+	_ "embed"
+)
+
+//go:embed builder/build.pkr.hcl
+var BuilderHCL string
